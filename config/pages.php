4df0de9cb355d32e237a738f2ca6ac960a4cd75d
Israel D. Canasa <raeldc@wizmediateam.com>
2010-02-02 23:01:42 +0800

    Fixed default configuration to use the default route.

--- config/pages.php
+++ config/pages.php
@@ -4,7 +4,7 @@ return array(
 	'mainmenu'	=> array(
 		array(
 			'title' 		=> 'Welcome',
-			'route'			=> 'statichtml',
+			'route'			=> 'default',
 			'params'		=> array('file' => 'welcome'),
 			'attributes' 	=> array(
 								'class' => 'home'
@@ -13,14 +13,14 @@ return array(
 		),
 		array(
 			'title' 		=> 'Register',
-			'route'			=> 'statichtml',
+			'route'			=> 'default',
 			'params'		=> array('file' => 'register'),
 			'attributes' 	=> array(),
 			'usergroups'	=> '0'
 		),
 		array(
 			'title' 		=> 'About Us',
-			'route'			=> 'statichtml',
+			'route'			=> 'default',
 			'params'		=> array('file' => 'about'),
 			'attributes' 	=> array(
 								'class' => 'login'
